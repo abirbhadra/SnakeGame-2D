a547efe03ed1fcc8fe8ad544dc3ab95a35d3a710
abirbhadra <58373108+abirbhadra@users.noreply.github.com>
2023-11-08 21:21:39 +0530

    updated code

---
+++ Assets/Scripts/SnakeController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SnakeController : MonoBehaviour
+{
+    [SerializeField]
+    private float speed = 2f;
+    [SerializeField]
+    private float rotationSpeed = 200f;
+    [SerializeField]
+    private float velX = 0f;
+
+    private void Update()
+    {
+        velX = Input.GetAxisRaw("Horizontal");
+    }
+    private void FixedUpdate()
+    {
+        // movement
+        transform.Translate(Vector2.up * speed * Time.fixedDeltaTime, Space.Self);
+        //rotate
+        transform.Rotate(Vector3.forward * -velX * rotationSpeed * Time.fixedDeltaTime);
+
+    }
+
+}
---
+++ Assets/Scripts/SnakeGrowthController.cs
@@ -0,0 +1,37 @@
+﻿/*using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SnakeGrowthController : MonoBehaviour
+{
+    public SnakeTailController snakeTailController;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Food"))
+        {
+            Destroy(collision.gameObject, 0.02f);
+            snakeTailController.AddTail();
+        }
+    }
+}*/
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SnakeGrowthController : MonoBehaviour
+{
+    public SnakeTailController snakeTailController;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("Collision detected");
+        if (collision.CompareTag("Food"))
+        {
+            Destroy(collision.gameObject);
+            Debug.Log("Food destroyed");
+            snakeTailController.AddTail();
+        }
+    }
+}
+
