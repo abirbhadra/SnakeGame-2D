b91f8f21e03640c032622063e262b0fee03ddd65
abirbhadra <58373108+abirbhadra@users.noreply.github.com>
2023-11-13 19:35:40 +0530

    basic setup

---
+++ Assets/Scripts/GameHandler.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameHandler : MonoBehaviour
+{
+   
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("GameHandler.Start");
+
+        GameObject snakeHeadGameObject = new GameObject();
+        SpriteRenderer snakeSpriteRenderer = snakeHeadGameObject.AddComponent<SpriteRenderer>();
+        snakeSpriteRenderer.sprite = GameAssets.i.SnakeHeadSprite;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
